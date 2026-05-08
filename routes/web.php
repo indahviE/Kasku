3eff6f41270e357611eb975b6b0f6a5142fe7a23
unknown <ve3rss@gmail.com>
2026-05-08 09:53:36 +0700

    update route siswa

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\KasController;
+use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
