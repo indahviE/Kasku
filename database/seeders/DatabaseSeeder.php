89a145953ad896573cda2d16ca8ca4e1ec677ba0
unknown <ve3rss@gmail.com>
2026-05-10 15:04:39 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminSeeder::class,
-            BendaharaSeeder::class,
+            UserSeeder::class,
             KelasSeeder::class,
             SiswaSeeder::class,
             TagihanSeeder::class,
